9df8675413f51ef41a2e656ffaff469fa0f2227f
Carol Greene <carolgreene3@gmail.com>
2018-06-16 13:50:11 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,10 +1,12 @@
 class SongsController < ApplicationController
   def index
     @songs = Song.all
+
   end
 
   def show
     @song = Song.find(params[:id])
+
   end
 
   def new
@@ -50,4 +52,3 @@ class SongsController < ApplicationController
     params.require(:song).permit(:title, :artist_name)
   end
 end
-
